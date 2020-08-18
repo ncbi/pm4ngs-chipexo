afd9a8393f8e93b48d6fd3677c0b642277899ec9
Vera Alvarez, Roberto <veraalva@ncbi.nlm.nih.gov>
2020-08-18 17:37:55 -0400

    first commit

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+import numpy as np
+import os
+import pandas
+import shutil
+import subprocess
+import sys
+import yaml
+from bioconda2biocontainer.update_cwl_docker_image import update_cwl_docker_from_tool_name
+from git import Repo
+from multiprocessing import Pool, cpu_count
+
+PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
+DATASET = '{{ cookiecutter.dataset_name }}'
+CWL_WORKFLOW_REPO = 'https://github.com/ncbi/cwl-ngs-workflows-cbb'
+DATASET_DIR = os.path.join(PROJECT_DIRECTORY, 'data', DATASET)
+WORK_DIR = os.environ.get('PM4NGS_WORK_DIR', None)
+SAMPLE_TABLE_FILE = os.environ.get('PM4NGS_SAMPLE_TABLE', None)
+COPY_RAWDATA = os.environ.get('PM4NGS_COPY_RAWDATA', None)
+
+
+def clone_git_repo():
+    """
+    Clone the git repo from the cookiecutter.cwl_workflow_repo to the bin directory
+    :return:
+    """
+    print('Cloning Git repo: {0} to {1}'.format(CWL_WORKFLOW_REPO,
+                                                os.path.join(PROJECT_DIRECTORY, 'bin', 'cwl')))
+    Repo.clone_from(CWL_WORKFLOW_REPO, os.path.join(PROJECT_DIRECTORY, 'bin', 'cwl'))
+
+
+def copy_directory(src, dest):
+    try:
+        shutil.copytree(src, dest)
+    # Directories are the same
+    except shutil.Error as e:
+        print('Directory not copied. Error: %s' % e)
+        sys.exit(-1)
+    # Any error saying that the directory doesn't exist
+    except OSError as e:
+        print('Directory not copied. Error: %s' % e)
+        sys.exit(-1)
+
+
+def copy_file(src, dest):
+    if os.path.exists(src):
+        try:
+            dest = os.path.join(dest, os.path.basename(src))
+            print(src + ' ==> ' + dest)
+            shutil.copyfile(src, dest)
+            return 0
+            # Directories are the same
+        except shutil.Error as e:
+            print('File not copied. Error: %s' % e)
+        # Any error saying that the directory doesn't exist
+        except OSError as e:
+            print('File not copied. Error: %s' % e)
+    else:
+        print('File {} not found'.format(src))
+    return -1
+
+
+def download_file(src, dest):
+    dest = os.path.join(dest, os.path.basename(src))
+    print('Downloading file {} to {}'.format(src, dest))
+    status = subprocess.run(['curl', '-o', dest, src], capture_output=True)
+    return status.returncode
+
+
+def rawdata_file_manager(file):
+    if file.startswith('/') and os.path.exists(file):
+        return copy_file(file, DATASET_DIR)
+    elif file.startswith('http') or file.startswith('ftp'):
+        return download_file(file, DATASET_DIR)
+    return copy_file(os.path.join(WORK_DIR, file), DATASET_DIR)
+
+
+def copy_rawdata_to_project():
+    if COPY_RAWDATA:
+        sample_table_file = os.path.join(DATASET_DIR, 'sample_table.csv')
+        sample_table = pandas.read_csv(sample_table_file, skip_blank_lines=True)
+        sample_table = sample_table.replace(np.nan, '', regex=True)
+        sample_table = sample_table[['sample_name', 'file', 'condition', 'replicate']]
+        print('{} files loaded\nUsing table:'.format(len(sample_table)))
+        print(sample_table)
+        files = sample_table[sample_table['file'] != '']['file'].unique()
+        if len(files) > 0:
+            print('Copying files in parallel using: {} CPUs'.format(cpu_count() - 1))
+            p = Pool(processes=cpu_count() - 1)
+            status = p.map(rawdata_file_manager, files)
+            for s in status:
+                if s != 0:
+                    print('Error copying raw data to project')
+                    sys.exit(-1)
+
+
+if __name__ == '__main__':
+
+    if SAMPLE_TABLE_FILE and COPY_RAWDATA and WORK_DIR:
+        conda_dependencies = os.path.join(PROJECT_DIRECTORY, 'requirements', 'conda-env-dependencies.yaml')
+        if os.path.exists(conda_dependencies):
+            if 'github.com' in CWL_WORKFLOW_REPO:
+                clone_git_repo()
+            elif os.path.exists(CWL_WORKFLOW_REPO):
+                print('Copying CWL directory {} to {}'.format(
+                    CWL_WORKFLOW_REPO, os.path.join(PROJECT_DIRECTORY, 'bin', 'cwl')
+                ))
+                copy_directory(CWL_WORKFLOW_REPO, os.path.join(PROJECT_DIRECTORY, 'bin', 'cwl'))
+            else:
+                print('CWL_WORKFLOW_REPO = {} not available.'.format(CWL_WORKFLOW_REPO))
+                print('Use Github URL or absolute path')
+                sys.exit(-1)
+
+            print('Updating CWLs dockerPull and SoftwareRequirement from: ' + conda_dependencies)
+            with open(conda_dependencies) as fin:
+                conda_env = yaml.load(fin, Loader=yaml.FullLoader)
+                if 'dependencies' in conda_env:
+                    for d in conda_env['dependencies']:
+                        update_cwl_docker_from_tool_name(d, os.path.join(PROJECT_DIRECTORY, 'bin', 'cwl'))
+
+            print('Copying file {}  to {}'.format(
+                SAMPLE_TABLE_FILE, os.path.join(DATASET_DIR, 'sample_table.csv')
+            ))
+            shutil.copyfile(SAMPLE_TABLE_FILE, os.path.join(DATASET_DIR, 'sample_table.csv'))
+            copy_rawdata_to_project()
+            print(' Done')
+        else:
+            print('No conda env dependency file in {0}'.format(conda_dependencies))
+            sys.exit(-1)
+    else:
+        print('Error reading user env')
+        print('PM4NGS_SAMPLE_TABLE: ' + str(SAMPLE_TABLE_FILE))
+        print('PM4NGS_COPY_RAWDATA: ' + str(COPY_RAWDATA))
+        print('PM4NGS_WORK_DIR: ' + str(WORK_DIR))
+        sys.exit(-1)
---
+++ {{cookiecutter.project_name}}/config/init.py
@@ -0,0 +1,155 @@
+import os
+import re
+import json
+import pandas
+import math
+import pickle
+import zipfile
+import uuid
+import distutils.spawn
+import numpy as np
+import scipy.stats as stats
+import seaborn as sns
+import itertools
+import networkx as nx
+
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+from IPython.display import HTML
+from IPython.display import display, Markdown, Latex
+
+from pathlib import Path
+
+from pm4ngs.jupyterngsplugin.utils.errors import check_cwl_command_log
+from pm4ngs.jupyterngsplugin.utils.run_command import run_command
+from pm4ngs.jupyterngsplugin.utils.working_dir import working_dir
+from pm4ngs.jupyterngsplugin.utils.yaml_utils import write_to_yaml
+from pm4ngs.jupyterngsplugin.utils.yaml_utils import load_from_yaml
+
+{% if cookiecutter.sequencing_technology == 'paired-end' %}
+from pm4ngs.jupyterngsplugin.utils.samples import write_sample_table_pe_to_yaml
+{% else %}
+from pm4ngs.jupyterngsplugin.utils.samples import write_sample_table_se_to_yaml
+{% endif %}
+
+###############################################################
+#
+#    Project global paths
+#
+###############################################################
+
+WORKDIR = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+CONFIG = os.path.join(WORKDIR,'config')
+DATA = os.path.join(WORKDIR,'data')
+BIN = os.path.join(WORKDIR,'bin')
+RESULTS = os.path.join(WORKDIR,'results')
+NOTEBOOKS = os.path.join(WORKDIR,'notebooks')
+SRC = os.path.join(WORKDIR,'src')
+TMP = os.path.join(WORKDIR,'tmp')
+
+###############################################################
+#
+#    Update genome files and indexes path
+#
+# If indexes and reference bed files does not exist can be created using 
+# the notebooks but you need to have writing permission in the GENOME dir
+#
+###############################################################
+
+GENOME = '{{ cookiecutter.genome_dir }}'
+GENOME_NAME = '{{ cookiecutter.genome_name }}'
+ALIGNER_INDEX = '{{ cookiecutter.aligner_index_dir }}'
+GENOME_FASTA = '{{ cookiecutter.genome_fasta }}'
+GENOME_GTF = '{{ cookiecutter.genome_gtf }}'
+GENOME_CHROMSIZES = '{{ cookiecutter.genome_chromsizes }}'
+
+if GENOME_NAME == GENOME:
+    GENOME = os.path.join(DATA, GENOME)
+    ALIGNER_INDEX = os.path.join(DATA, ALIGNER_INDEX)
+    GENOME_FASTA = os.path.join(DATA, GENOME_FASTA)
+    GENOME_GTF = os.path.join(DATA, GENOME_GTF)
+
+###############################################################
+#
+#    Dataset (experiment) to analyze
+#
+# The path is $WORKDIR/data/$DATASET
+#
+# To use multiple datasets (experiments) this variable should be overwritten
+# in the notebooks
+#
+###############################################################
+
+DATASET = '{{ cookiecutter.dataset_name }}'
+{% if cookiecutter.is_data_in_SRA == 'y' %}
+
+HOME_DIR = os.path.expanduser("~")
+NCBI_DIR = os.path.join(HOME_DIR, '.ncbi')
+SRA_TOOLS_CONFIG = os.path.join(NCBI_DIR, 'user-settings.mkfg')
+if not os.path.exists(SRA_TOOLS_CONFIG):
+    if not os.path.exists(NCBI_DIR):
+        print('Creating ncbi dir: ' + NCBI_DIR)
+        os.mkdir(NCBI_DIR)
+
+    if not os.path.exists(SRA_TOOLS_CONFIG):
+        print('Creating sra-tools config file: ' + SRA_TOOLS_CONFIG)
+        with open(SRA_TOOLS_CONFIG, 'w') as fout:
+            fout.write('/LIBS/GUID = "{}"\n'.format(uuid.uuid4()))
+            fout.write('/libs/cloud/report_instance_identity = "true"\n')
+
+
+{% if cookiecutter.create_demo == 'y' %}
+IS_DEMO = True
+{% else %}
+IS_DEMO = False
+{% endif %}
+{% else %}
+IS_DEMO = False
+{% endif %}
+
+###############################################################
+#
+#    Docker configuration
+#
+###############################################################
+
+{% if cookiecutter.use_docker == 'y' %}
+DOCKER = True
+{% else %}
+DOCKER = False
+{% endif %}
+
+###############################################################
+#
+#    cwl-runner with absolute path if necesary 
+#
+###############################################################
+
+CWLRUNNER_TOOL = 'cwltool'
+CWLRUNNER_TOOL_PATH = distutils.spawn.find_executable(CWLRUNNER_TOOL)
+if not CWLRUNNER_TOOL_PATH:
+    print('WARNING: %s not in path' % (CWLRUNNER_TOOL))
+    print('Install: cwltool')
+else:
+    CWLRUNNER = CWLRUNNER_TOOL_PATH
+if not DOCKER:
+    CWLTOOL_DEPS = os.path.join(BIN,'cwltool_deps')
+    CWLRUNNER +=' --no-container --beta-conda-dependencies --beta-dependencies-directory ' + CWLTOOL_DEPS
+    try:
+        TRIMMOMATIC_ADAPTERS = str(next(Path(os.path.join(CWLTOOL_DEPS, '_conda', 'envs' )).rglob('__trimmomatic*//share/trimmomatic/adapters')))
+    except:
+        TRIMMOMATIC_ADAPTERS = ''
+else:
+    CWLRUNNER += ' --no-read-only --beta-use-biocontainers'
+    TRIMMOMATIC_ADAPTERS = '/usr/local/share/trimmomatic/adapters/'
+
+###############################################################
+
+
+CWLURL = os.path.join(BIN,'cwl')
+CWLTOOLS = os.path.join(CWLURL, 'tools')
+CWLWORKFLOWS = os.path.join(CWLURL, 'workflows')
+
+CWLRUNNER = CWLRUNNER + ' --parallel --on-error continue --rm-tmpdir --tmp-outdir-prefix=' + TMP + '/ --tmpdir-prefix=' + TMP + '/ '
