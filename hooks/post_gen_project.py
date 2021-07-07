089bad402ed6fd843313c8f65902e7b697a2438b
Vera Alvarez, Roberto <veraalva@ncbi.nlm.nih.gov>
2021-07-07 15:42:39 -0400

    Updating post gen hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
-import numpy as np
 import os
-import pandas
 import shutil
-import subprocess
 import sys
+
 import yaml
 from bioconda2biocontainer.update_cwl_docker_image import update_cwl_docker_from_tool_name
-from git import Repo
-from multiprocessing import Pool, cpu_count
+from pm4ngs.utils import clone_git_repo
+from pm4ngs.utils import copy_directory
+from pm4ngs.utils import copy_rawdata_to_project
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 DATASET = '{{ cookiecutter.dataset_name }}'
@@ -18,86 +17,7 @@ WORK_DIR = os.environ.get('PM4NGS_WORK_DIR', None)
 SAMPLE_TABLE_FILE = os.environ.get('PM4NGS_SAMPLE_TABLE', None)
 COPY_RAWDATA = os.environ.get('PM4NGS_COPY_RAWDATA', None)
 
-
-def clone_git_repo():
-    """
-    Clone the git repo from the cookiecutter.cwl_workflow_repo to the bin directory
-    :return:
-    """
-    print('Cloning Git repo: {0} to {1}'.format(CWL_WORKFLOW_REPO,
-                                                os.path.join(PROJECT_DIRECTORY, 'bin', 'cwl')))
-    Repo.clone_from(CWL_WORKFLOW_REPO, os.path.join(PROJECT_DIRECTORY, 'bin', 'cwl'))
-
-
-def copy_directory(src, dest):
-    try:
-        shutil.copytree(src, dest)
-    # Directories are the same
-    except shutil.Error as e:
-        print('Directory not copied. Error: %s' % e)
-        sys.exit(-1)
-    # Any error saying that the directory doesn't exist
-    except OSError as e:
-        print('Directory not copied. Error: %s' % e)
-        sys.exit(-1)
-
-
-def copy_file(src, dest):
-    if os.path.exists(src):
-        try:
-            dest = os.path.join(dest, os.path.basename(src))
-            print(src + ' ==> ' + dest)
-            shutil.copyfile(src, dest)
-            return 0
-            # Directories are the same
-        except shutil.Error as e:
-            print('File not copied. Error: %s' % e)
-        # Any error saying that the directory doesn't exist
-        except OSError as e:
-            print('File not copied. Error: %s' % e)
-    else:
-        print('File {} not found'.format(src))
-    return -1
-
-
-def download_file(src, dest):
-    dest = os.path.join(dest, os.path.basename(src))
-    print('Downloading file {} to {}'.format(src, dest))
-    status = subprocess.run(['curl', '-o', dest, src], capture_output=True)
-    return status.returncode
-
-
-def rawdata_file_manager(file):
-    if file.startswith('/') and os.path.exists(file):
-        return copy_file(file, DATASET_DIR)
-    elif file.startswith('http') or file.startswith('ftp'):
-        return download_file(file, DATASET_DIR)
-    return copy_file(os.path.join(WORK_DIR, file), DATASET_DIR)
-
-
-def copy_rawdata_to_project():
-    if COPY_RAWDATA == 'True':
-        sample_table_file = os.path.join(DATASET_DIR, 'sample_table.csv')
-        sample_table = pandas.read_csv(sample_table_file, skip_blank_lines=True)
-        sample_table = sample_table.replace(np.nan, '', regex=True)
-        sample_table = sample_table[['sample_name', 'file', 'condition', 'replicate']]
-        print('{} files loaded\nUsing table:'.format(len(sample_table)))
-        print(sample_table)
-        files = []
-        for f in sample_table[sample_table['file'] != '']['file'].unique():
-            files.extend(f.split('|'))
-        if len(files) > 0:
-            print('Copying files in parallel using: {} CPUs'.format(cpu_count() - 1))
-            p = Pool(processes=cpu_count() - 1)
-            status = p.map(rawdata_file_manager, files)
-            for s in status:
-                if s != 0:
-                    print('Error copying raw data to project')
-                    sys.exit(-1)
-
-
 if __name__ == '__main__':
-
     if SAMPLE_TABLE_FILE and COPY_RAWDATA and WORK_DIR:
         conda_dependencies = os.path.join(PROJECT_DIRECTORY, 'requirements', 'conda-env-dependencies.yaml')
         if os.path.exists(conda_dependencies):
