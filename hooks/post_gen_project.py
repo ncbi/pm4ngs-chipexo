c1cd9f97fa56ae5c1f4edc4a5ed3df7b2f5e3407
Vera Alvarez, Roberto <veraalva@ncbi.nlm.nih.gov>
2021-07-07 16:45:06 -0400

    Adding a standard hook for similar templates

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,9 +4,4 @@ import os
 from pm4ngs.utils import main_hook_standard_template
 
 if __name__ == '__main__':
-    main_hook_standard_template(os.environ.get('PM4NGS_SAMPLE_TABLE', None),
-                                os.environ.get('PM4NGS_COPY_RAWDATA', None),
-                                os.environ.get('PM4NGS_WORK_DIR', None),
-                                '{{ cookiecutter.dataset_name }}',
-                                os.path.realpath(os.path.curdir),
-                                'https://github.com/ncbi/cwl-ngs-workflows-cbb')
+    main_hook_standard_template('{{ cookiecutter.dataset_name }}')
